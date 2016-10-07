abca72dbba1be4a7d49de241bd7cc3863900e3ff
Paulo Junior <paulo.avelinojunior@outlook.com>
2016-10-07 17:43:29 -0300

    Commit com settings SGSI

---
+++ SGSI.Web/Business/SGSIBusiness.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SGSI.Web.Data;
+
+namespace SGSI.Web.Business
+{
+    public class SGSIBusiness
+    {
+
+        public int AdicionarUsuario(string nome, string email, string senha) {
+
+            DBSGSI db = new DBSGSI();
+
+            return db.InsereNovoUsuario(nome, email, senha);
+        }
+
+    }
+}
---
+++ SGSI.Web/Data/DBSGSI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using SGSI.Data;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace SGSI.Web.Data
+{
+      public class DBSGSI
+    {
+        string stringConexao = ConfigurationManager.ConnectionStrings["TccConnectionString"].ConnectionString;
+
+        public int InsereNovoUsuario(string nome, string email, string senha)
+        {
+
+
+            int p_retorno = 0;
+
+            SqlHelperFactory.ExecuteNonQuery(
+                SGSI.Settings.Settings.Default.InstanceDB,
+                "InsereUsuario",
+                new object[] {
+                nome,
+                email,
+                senha,
+                p_retorno},
+                delegate (Database db, DbCommand commandWrapper)
+                {
+                    p_retorno = Convert.ToInt32(db.GetParameterValue(commandWrapper, "p_retorno"));
+                }
+                );
+
+
+            return p_retorno;
+
+        }
+
+    }
+}
